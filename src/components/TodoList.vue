c84df960c4cc7ed1b0bc8003bf74203f76d8b7dd
Grant Williams <grant.williams@credera.com>
2020-04-01 16:02:20 -0500

    Add list of TodoItems to Dashboard

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="todo-list">
+    <todo-item
+      v-bind:key="todo.id"
+      v-for="todo in todos"
+      v-on:delete-todo="$emit('delete-todo', todo.id)"
+      :todo="todo"
+    ></todo-item>
+  </ul>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem.vue";
+export default {
+  name: "TodoList",
+  components: {
+    "todo-item": TodoItem
+  },
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+.todo-list {
+  margin-top: 10px;
+}
+ul {
+  max-height: 50vh;
+  overflow: scroll;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="dashboard">
-    <h1>This is the dashboard page</h1>
+    <h1>This is the Dashboard</h1>
+    <p>Add and manage tasks within the todo list below.</p>
+    <div class="todo-container">
+      <add-todo v-on:add-todo="addTodo"></add-todo>
+      <todo-list :todos="todos" v-on:delete-todo="deleteTodo"></todo-list>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+import AddTodo from "@/components/AddTodo.vue";
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    "add-todo": AddTodo,
+    "todo-list": TodoList
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=4")
+      .then(response => (this.todos = response.data))
+      .catch(error => console.log(error));
+  }
+};
+</script>
+
+<style scoped>
+.todo-container {
+  width: 50vw;
+  height: 50vh;
+  margin: auto;
+}
+</style>
