68c2b303142bf72541961b300fbdb9d6113a5712
Grant Williams <grant.williams@credera.com>
2020-04-02 11:01:12 -0500

    Correct Linter error

--- tests/unit/Dashboard.spec.js
+++ tests/unit/Dashboard.spec.js
@@ -2,8 +2,8 @@ import { mount } from "@vue/test-utils";
 import Dashboard from "@/views/Dashboard.vue";
 
 describe("Dashboard.vue", () => {
-	it("renders Dashboard header", () => {
-		const wrapper = mount(Dashboard);
-		expect(wrapper.contains("h1")).toBe(true);
-	});
+  it("renders Dashboard header", () => {
+    const wrapper = mount(Dashboard);
+    expect(wrapper.contains("h1")).toBe(true);
+  });
 });
--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -3,18 +3,18 @@ import Home from "@/views/Home.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("Home.vue", () => {
-	it("renders Home header", () => {
-		const wrapper = mount(Home);
-		expect(wrapper.contains("h1")).toBe(true);
-	});
+  it("renders Home header", () => {
+    const wrapper = mount(Home);
+    expect(wrapper.contains("h1")).toBe(true);
+  });
 });
 
 describe("HelloWorld.vue", () => {
-	it("renders props.msg when passed", () => {
-		const msg = "new message";
-		const wrapper = shallowMount(HelloWorld, {
-			propsData: { msg }
-		});
-		expect(wrapper.text()).toMatch(msg);
-	});
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
 });
